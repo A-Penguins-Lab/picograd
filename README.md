867344be09d888e2544a98e21f44bed6859b2d99
Rishik00 <96735720+Rishik00@users.noreply.github.com>
2025-02-27 21:39:21 +0530

    1. added multidim arrays using indexing for the arrays 2.updated the readme

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 ## PicoGrad  
-A minimal implementation of PyTorch's autograd with full GPU compatibility  
+A minimal implementation of PyTorch's autograd with full GPU compatibility 
+
+PicoGrad is inspired from tinygrad and micrograd. I am building a scaled down version of tinygrad and autograd annd a scaled up version of micrograd to support GPUs and multidimensional tensors. 
+
+It is a lot of fun, the primary objective behind this project is to document all my findings on building with GPUs and post in a blog. 
+
+Once fully built, picograd will have: 
+1. A better name
+2. Support for GPUs using triton/CUDA (yet to be researched)
+3. autograd like functionality
+
+## Is there anything that sets this apart? 
+Absolutely not, and its always best to use pytorch or tinygrad if we want reliable functionality. This is a toy project that is purely meant for learning purposes. 
 
 ## To-Do List  
-- [ ] Set up the project structure  
+- [x] Set up the project structure  
 - [ ] Set up a blog  
 - [ ] Build the Tensor object  
 - [ ] Write unit tests  
--- pico.py
+++ pico.py
@@ -1,4 +1,3 @@
-import torch
 import time
 import numpy as np
 import cProfile
@@ -8,13 +7,17 @@ import numpy as np
 class Tensor:
     def __init__(self, data, label='', _op='', _children=()):
         
+        ## This if for CPU
         self.data = np.array(data)
         self.grad = 0.0
         self.label = label
-
+        
         self._backward = lambda: None
         self._op = _op
         self._prev = set(_children)
+
+    def T(self):
+        pass
 
     def __add__(self, other):
         # assert (type(other) == Tensor, "Other is a tensor, op=add")
@@ -34,20 +37,34 @@ class Tensor:
 
         return out
 
+    ## Indexing and slicing for tensor object
+    def __getitem__(self, indices=None):
+        if indices is None:
+            return self.data
+
+        if isinstance(indices, (int, slice, tuple)):
+            return self.data[indices]
+
+        raise TypeError(f"Invalid index type: {type(indices)}")
+
     def backward(self):
         pass
 
     def __repr__(self):
-        return f"tensor created with data: {self.data}"
+        return str(self.data)
+
+def tensor():
+    pass
 
 if __name__ == "__main__":
     start_time = time.time()
-    a = Tensor(2.0, label='a')
-    b = Tensor(3.5, label='b')
+    a = Tensor([2.0, 3.4], label='a')
+    b = Tensor([3.5, 3.5], label='b')
     c = a + b
-    
+
     end_time = time.time()
 
     ## This needs some speeding up
     print(end_time - start_time)
-    print(c, c.data, type(c.data))
+    print(c[1])
+    print(c, 'data: ', c.data, 'data type: ', type(c.data))
